8524c35a2bbc1964d5b829c87bf6abc81d193390
shellchen <shellchen@tencent.com>
2020-01-18 21:43:06 +0800

    fix | 修正nodejs端发起请求时sslv3不支持的报错

--- src/api/blog.js
+++ src/api/blog.js
@@ -1,10 +1,13 @@
 /*
  * @Author: shallwe
  * @Date: 2019-12-10 16:35:48
- * @LastEditTime: 2019-12-10 16:51:10
- * @LastEditors: shallwe
+ * @LastEditTime : 2020-01-18 21:35:18
+ * @LastEditors  : shallwe
  */
 import request from '../utils/request'
+
+const tls = require('tls')
+tls.DEFAULT_ECDH_CURVE = 'auto'
 
 export function getBlogList(params) {
     return request({
@@ -15,7 +18,6 @@ export function getBlogList(params) {
 }
 
 export function getBlogDetail(params) {
-    params = params || {}
     return request({
         url: `/blog/get/${params.blogId}`,
         method: 'get',
